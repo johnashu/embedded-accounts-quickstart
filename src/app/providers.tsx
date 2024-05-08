dc4577849ff8c88f0a0f14f435ecf8e30eecab49
Maffaz <john@madnfts.io>
2024-05-08 12:46:06 +0200

    [UPD] chains

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,7 +2,7 @@
 
 import { createConfig } from "@alchemy/aa-alchemy/config";
 import { AlchemyAccountProvider } from "@alchemy/aa-alchemy/react";
-import { arbitrumSepolia } from "@alchemy/aa-core";
+import { sepolia } from "@alchemy/aa-core";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { PropsWithChildren, Suspense } from "react";
 
@@ -10,7 +10,7 @@ export const Providers = (props: PropsWithChildren) => {
   const queryClient = new QueryClient();
   const config = createConfig({
     rpcUrl: "/api/rpc",
-    chain: arbitrumSepolia,
+    chain: sepolia,
   });
 
   return (
